c378a0d3e39985a7e3d70c2a85c8188ee651b21d
naksuns <nschoi79@gmail.com>
2013-12-18 01:01:55 +0900

    Add java source and Edit xml, project config.

--- src/main/java/org/nerdronix/springnetty/cfg/SpringConfig.java
+++ src/main/java/org/nerdronix/springnetty/cfg/SpringConfig.java
@@ -1,12 +1,35 @@
 package org.nerdronix.springnetty.cfg;
 
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
+
+import java.net.InetSocketAddress;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+import org.nerdronix.springnetty.handlers.StringProtocolInitializer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
+
+/**
+ * This class contains the bean definitions for this netty server.
+ * These beans are autowired into the respective java classes in org.nerdronix.* packages
+ * using component scan feature of spring.
+ * Properties are injected using the PropertySource.
+ *
+ */
 @Configuration
 @ComponentScan("org.nerdronix")
 @PropertySource("classpath:netty-server.properties")
@@ -32,11 +55,66 @@ public class SpringConfig {
 	
 	@Autowired
 	@Qualifier("springProtocolInitializer")
-	private StringProtocolInitailizer protocolInitializer;
+	private StringProtocolInitializer protocolInitializer;
 	
 	@SuppressWarnings("unchecked")
 	@Bean(name="serverBootstrap")
 	public ServerBootstrap bootstrap() {
+		ServerBootstrap b = new ServerBootstrap();
+		b.group(bossGroup(), workerGroup())
+		 .channel(NioServerSocketChannel.class)
+		 .childHandler(protocolInitializer);
 		
+		Map<ChannelOption<?>, Object> tcpChannelOptions = tcpChannelOptions();
+		Set<ChannelOption<?>> keySet = tcpChannelOptions.keySet();
+		
+		for(@SuppressWarnings("rawtypes")ChannelOption option : keySet) {
+			b.option(option, tcpChannelOptions.get(option));
+		}
+		
+		return b;
+	}
+	
+	@Bean(name = "bossGroup", destroyMethod = "shutdownGracefully")
+	public NioEventLoopGroup bossGroup() {
+		return new NioEventLoopGroup(bossCount);
+	}
+	
+	@Bean(name = "workerGroup", destroyMethod = "shutdownGracefully")
+	public NioEventLoopGroup workerGroup() {
+		return new NioEventLoopGroup(workerCount);
+	}
+	
+	@Bean(name = "tcpSocketAddress")
+	public InetSocketAddress tcpPort() {
+		return new InetSocketAddress(tcpPort);
+	}
+	
+	@Bean(name = "tcpChannelOptions")
+	public Map<ChannelOption<?>, Object> tcpChannelOptions() {
+		Map<ChannelOption<?>, Object> options = new HashMap<ChannelOption<?>, Object>();
+		options.put(ChannelOption.SO_KEEPALIVE, keepAlive);
+		options.put(ChannelOption.SO_BACKLOG, backlog);
+		
+		return options;
+	}
+	
+	@Bean(name = "stringEncoder")
+	public StringEncoder stringEncoder() {
+		return new StringEncoder();
+	}
+	
+	@Bean(name = "stringDecoder")
+	public StringDecoder stringDecoder() {
+		return new StringDecoder();
+	}
+	
+	/**
+	 * Necessary to make the Value annotations work.
+	 * @return
+	 */
+	@Bean
+	public static PropertySourcesPlaceholderConfigurer propertyPlaceholderConfigurer() {
+		return new PropertySourcesPlaceholderConfigurer();
 	}
 }
---
+++ src/main/java/org/nerdronix/springnetty/server/Main.java
@@ -0,0 +1,21 @@
+package org.nerdronix.springnetty.server;
+
+import org.nerdronix.springnetty.cfg.SpringConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
+
+public class Main {
+	
+	public static final Logger logger = LoggerFactory.getLogger(Main.class);
+	
+	public static void main(String[] args) {
+		logger.debug("Starting application context.");
+		
+		@SuppressWarnings("resource")
+		AbstractApplicationContext ctx = new AnnotationConfigApplicationContext(SpringConfig.class);
+		ctx.registerShutdownHook();
+	}
+
+}
